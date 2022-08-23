b552d32cd564b719cab5c2ed63f15aa8b1296cc4
cnerh <cynetember@gmail.com>
2022-08-24 01:34:08 +0200

    final

--- CoMute/Content/js/carpoolview.js
+++ CoMute/Content/js/carpoolview.js
@@ -1,53 +1,71 @@
 ﻿
 ; (function (root, $) {
-    $(document).ready(function(){
+    //var table;
+
+    $(document).ready(function () {
 
         $.get('/Carpool/get', function (data) {
-          
+           // $("#cartableView tr").detach();
+            
             $.each(data, function (index, value) {
-                
-                $('#cartableView tbody:eq(1)').append('<tr><td>' + value.DepartureTime + '</td><td>' + value.ExpectedArrivaltime +
-                   '</td><td>' + value.Origin + '</td><td>' + value.DaysAvailable + '</td><td>' + value.Destination + '</td><td>' + value.AvailableSeats + '</td><td>' + value.Owner + '</td><td>' + value.Notes + '</td><td> <button id="join" calss="btn btn-danger btnEdit" >Join</button> </td></tr>');
 
 
+                $('#cartableView tbody:eq(0)').append('<tr><td>' +
+                    value.DepartureTime + '</td><td>' +
+                    value.ExpectedArrivaltime +
+                    '</td><td>' + value.Origin +
+                    '</td><td>' + value.DaysAvailable +
+                    '</td><td>' + value.Destination + '</td><td>' +
+                    value.AvailableSeats + '</td><td>' + value.Owner +
+                    '</td><td>' + value.Notes +
+                    '</td><td> <button id="join" value="' + value.CarPoolID + '" calss="btn btn - danger btnEdit" >Join</button> </td></tr>');
 
-                if (value.CarPoolID != null && section )
+                if (value.CarPoolID != null) {
+
                     $("#join").html("leave");
 
+                }
+               
 
-                $("#join").click(function () {
-
-                
-                    var requestdata = { CarPoolID: value.CarPoolID }
-
-                    $.post('/RegistrationRequest/Assign',
-                        requestdata, function (data) {
-                            //  $("#join").text = "leave";
-                            var pswd = $('#inputPassword').val();
-                           // $("#join").html("leave");
-
-
-                            var btnvalue = $("#join").html();
-                            if (btnvalue == 'Join') {
-                                $("#join").html("leave");
-                            }
-
-
-                            // $("#join").prop("value", "leave");
-                            else {
-                                $("#join").html("Join");
-                               
-                            }
-
-                           
-
-
-                        })
-
-                })
             })
 
-        })
+            
+
+
+            $('body').on('click', '#join', function ()
+           // $("#join").click(function ()
+            {
+         
+              
+                var CarID = $("#join").val();
+
+                var requestdata = { CarPoolID: CarID }
+
+                $.post('/RegistrationRequest/Assign',
+                    requestdata, function (data) {
+                        //  $("#join").text = "leave";
+                        // $("#join").html("leave");
+
+
+                        //var btnvalue = $("#join").html();
+                        //if (btnvalue == 'Join') {
+                        //    $("#join").html("leave");
+                        //}
+
+
+                        //// $("#join").prop("value", "leave");
+                        //else {
+                        //    $("#join").html("Join");
+
+                        //}
+
+                    })
+            })
+
+        //    })
+
+      
+         
 
         $("#Search").click(function () {
      
@@ -70,7 +88,7 @@
 
         })
 
-
+        });
    })
   
 })(window, jQuery);
